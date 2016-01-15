8079b770420aee5edb3c963a1e0efaa5b4ab871f
Fahien <sir.fahien@gmail.com>
2016-01-15 17:25:40 +0100

    PlanetComponent refactoring

--- core/src/me/fahien/spacefloat/component/PlanetComponent.java
+++ core/src/me/fahien/spacefloat/component/PlanetComponent.java
@@ -8,9 +8,6 @@ import com.badlogic.gdx.math.Vector3;
  * @author Fahien
  */
 public class PlanetComponent extends RigidbodyComponent {
-	public final static short PLANET_GROUP = 1 << 2; // 4
-	private final static short PLANET_MASK = 7; // The spaceship
-
 	@Override
 	public void createRigidbody() {
 		super.createRigidbody();
